85ef7c6ec5d5fe1d1d9af42a1f19383c9f077d3b
kaiiruu <120348434+kaiiruu@users.noreply.github.com>
2024-08-27 18:29:05 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'May sasabihin ako'.split('')
+const text = 'HEY BABY!!!'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
